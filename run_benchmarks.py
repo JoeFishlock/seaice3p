fa14a649617d67f60eca32910f54932d7aec4451
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-04-19 16:56:58 +0100

    add scripts to run benchmark simulation for three solvers and compare
    LXF LXFImplicit and lagged upwind run for yearly forcing with small
    bubbles and plots to compare gas fraction.

---
+++ run_benchmarks.py
@@ -0,0 +1,26 @@
+"""Load and run all simulation configuration yaml files in benchmarks/ and save data.
+
+log if the simulation runs or crashes.
+"""
+from glob import glob
+from celestine.params import Config
+from celestine.run_simulation import solve
+from celestine.logging_config import logger, log_time
+from celestine.__init__ import __version__
+
+logger.info(f"Celestine version {__version__}")
+
+test_path = "benchmarks/"
+
+logger.info(f"Running test simulations in {test_path}")
+
+config_paths = glob(f"{test_path}*.yml")
+for path in config_paths:
+    cfg = Config.load(path)
+    logger.info(f"Running {cfg.name}")
+    try:
+        status, duration = solve(cfg)
+        log_time(logger, duration, message=f"{cfg.name} ran in ")
+    except Exception as e:
+        logger.error(f"{cfg.name} crashed")
+        logger.error(f"{e}")
