afd11d5dfa2694ae8980cd146f3def04c74a9578
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-05-17 21:18:36 +0100

    Change lagged solver to use center grid quantities and return new state.

--- celestine/solvers/lagged_solver.py
+++ celestine/solvers/lagged_solver.py
@@ -1,20 +1,9 @@
 import numpy as np
-from celestine.boundary_conditions import calculate_enthalpy_from_temp
 from celestine.forcing import get_temperature_forcing
-from celestine.enthalpy_method import (
-    calculate_enthalpy_method,
-    get_phase_masks,
-    calculate_temperature,
-    calculate_liquid_fraction,
-    calculate_gas_fraction,
-)
 from celestine.grids import (
-    get_difference_matrix,
-    get_number_of_timesteps,
     upwind,
-    geometric,
-    initialise_grids,
-    average,
+    add_ghost_cells,
+    centers_to_edges,
 )
 from celestine.velocities import (
     calculate_velocities,
@@ -44,85 +33,87 @@ class LaggedUpwindSolver(SolverTemplate):
 
         top_temperature = get_temperature_forcing(time, cfg)
         new_top_temperature = get_temperature_forcing(new_time, cfg)
+        far_temp = cfg.boundary_conditions_config.far_temp
+        far_gas_sat = cfg.boundary_conditions_config.far_gas_sat
+        far_bulk_salt = cfg.boundary_conditions_config.far_bulk_salinity
 
-        top_enthalpy = calculate_enthalpy_from_temp(0, 0, top_temperature, cfg)
-        new_top_enthalpy = calculate_enthalpy_from_temp(0, 0, new_top_temperature, cfg)
+        state.top_temperature = top_temperature
 
         # calculate temperature, salinity etc for state on grid centers
         state.calculate_enthalpy_method(cfg)
-        liquid_fraction_centers = state.liquid_fraction
+
+        enthalpy = state.enthalpy
+        salt = state.salt
+        gas = state.gas
         pressure = state.pressure
         liquid_fraction = state.liquid_fraction
+        temperature = state.temperature
+        liquid_salinity = state.liquid_salinity
+        gas_fraction = state.gas_fraction
+        dissolved_gas = state.dissolved_gas
+
+        dissolved_gas_ghosts = add_ghost_cells(dissolved_gas, bottom=far_gas_sat, top=1)
+        gas_fraction_ghosts = add_ghost_cells(gas_fraction, bottom=0, top=0)
+        gas_ghosts = add_ghost_cells(gas, bottom=chi * far_gas_sat, top=chi)
+
+        liquid_salinity_ghosts = add_ghost_cells(
+            liquid_salinity, bottom=far_bulk_salt, top=liquid_salinity[-1]
+        )
+
+        temperature_ghosts = add_ghost_cells(
+            temperature, bottom=far_temp, top=top_temperature
+        )
+        enthalpy_ghosts = add_ghost_cells(enthalpy, bottom=far_temp, top=enthalpy[-1])
+        salt_ghosts = add_ghost_cells(salt, bottom=far_bulk_salt, top=salt[-1])
+
+        liquid_fraction_edges = centers_to_edges(liquid_fraction)
 
         Vg, Wl, V = calculate_velocities(liquid_fraction, pressure, D_g, cfg)
 
-        new_enthalpy = np.zeros((I + 2,))
-        new_salt = np.zeros((I + 2,))
-        new_gas = np.zeros((I + 2,))
-        new_pressure = np.zeros((I + 2,))
+        new_enthalpy = np.zeros((I,))
+        new_salt = np.zeros((I,))
+        new_gas = np.zeros((I,))
+        new_pressure = np.zeros((I,))
 
-        new_enthalpy[-1] = new_top_enthalpy
-        new_enthalpy[0] = cfg.boundary_conditions_config.far_temp
-        new_salt[-1] = 0
-        new_salt[0] = 0
-        new_gas[-1] = 0
-        new_gas[0] = chi
-
-        new_enthalpy[1:-1] = enthalpy[1:-1] + timestep * (
-            np.matmul(D_e, np.matmul(D_g, temperature))
-            - np.matmul(D_e, upwind(temperature, Wl))
-            - np.matmul(D_e, upwind(enthalpy, V))
+        new_enthalpy = enthalpy + timestep * (
+            np.matmul(D_e, np.matmul(D_g, temperature_ghosts))
+            - np.matmul(D_e, upwind(temperature_ghosts, Wl))
+            - np.matmul(D_e, upwind(enthalpy_ghosts, V))
         )
-        new_salt[1:-1] = salt[1:-1] + timestep * (
+        new_salt = salt + timestep * (
             (1 / cfg.physical_params.lewis_salt)
             * np.matmul(
-                D_e, geometric(liquid_fraction) * np.matmul(D_g, liquid_salinity)
+                D_e, liquid_fraction_edges * np.matmul(D_g, liquid_salinity_ghosts)
             )
-            - np.matmul(D_e, upwind(salt, V))
-            - np.matmul(D_e, upwind(liquid_salinity + C, Wl))
+            - np.matmul(D_e, upwind(salt_ghosts, V))
+            - np.matmul(D_e, upwind(liquid_salinity_ghosts + C, Wl))
         )
-        new_gas[1:-1] = gas[1:-1] + timestep * (
+        new_gas = gas + timestep * (
             (chi / cfg.physical_params.lewis_gas)
-            * np.matmul(D_e, geometric(liquid_fraction) * np.matmul(D_g, dissolved_gas))
-            - np.matmul(D_e, upwind(gas, V))
-            - np.matmul(D_e, upwind(gas_fraction, Vg))
-            - np.matmul(D_e, upwind(chi * dissolved_gas, Wl))
+            * np.matmul(
+                D_e, liquid_fraction_edges * np.matmul(D_g, dissolved_gas_ghosts)
+            )
+            - np.matmul(D_e, upwind(gas_ghosts, V))
+            - np.matmul(D_e, upwind(gas_fraction_ghosts, Vg))
+            - np.matmul(D_e, upwind(chi * dissolved_gas_ghosts, Wl))
         )
 
-        new_phase_masks = get_phase_masks(
+        new_state = State(
+            new_time,
             new_enthalpy,
             new_salt,
             new_gas,
-            cfg,
+            top_temperature=new_top_temperature,
         )
-        (
-            new_temperature,
-            new_liquid_fraction,
-            new_gas_fraction,
-            _,
-            new_liquid_salinity,
-            new_dissolved_gas,
-        ) = calculate_enthalpy_method(
-            new_enthalpy, new_salt, new_gas, cfg, new_phase_masks
-        )
-        new_permeability = calculate_absolute_permeability(
-            geometric(new_liquid_fraction)
-        )
+        new_state.calculate_enthalpy_method(cfg)
+
+        new_liquid_fraction = new_state.liquid_fraction
+        new_gas_fraction = new_state.gas_fraction
+        new_permeability = calculate_absolute_permeability(new_liquid_fraction)
 
         new_pressure = solve_pressure_equation(
             gas_fraction, new_gas_fraction, new_permeability, timestep, D_e, D_g, cfg
         )
+        new_state.pressure = new_pressure
 
-        step, _, _, _ = initialise_grids(self.I)
-
-        CFL_timesteps = (
-            cfg.numerical_params.CFL_limit
-            * step
-            / np.where(np.abs(upwind(gas_fraction, Vg)) > 0, np.abs(Vg), 1e-10)
-        )
-        Courant_timesteps = cfg.numerical_params.Courant_limit * step**2
-        CFL_min_timestep = np.min(CFL_timesteps)
-        Courant_min_timestep = np.min(Courant_timesteps)
-        min_timestep = min(CFL_min_timestep, Courant_min_timestep)
-
-        return new_enthalpy, new_salt, new_gas, new_pressure, new_time, min_timestep
+        return new_state
