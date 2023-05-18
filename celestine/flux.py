33749a79db98855e52428d51fbcbecf3c75a7512
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-05-18 10:58:10 +0100

    Functions to calculate salt and gas fluxes

--- celestine/flux.py
+++ celestine/flux.py
@@ -1,5 +1,5 @@
 import numpy as np
-from celestine.grids import upwind
+from celestine.grids import upwind, geometric
 
 
 def calculate_conductive_heat_flux(temperature, D_g):
@@ -34,3 +34,68 @@ def calculate_heat_flux(state_BCs, Wl, V, D_g):
         + calculate_frame_advection_heat_flux(enthalpy, V)
     )
     return heat_flux
+
+
+def calculate_diffusive_salt_flux(liquid_salinity, liquid_fraction, D_g, cfg):
+    """Take liquid salinity and liquid fraction on ghost grid and interpolate liquid
+    fraction geometrically"""
+    lewis_salt = cfg.physical_params.lewis_salt
+    return (
+        -(1 / lewis_salt) * geometric(liquid_fraction) * np.matmul(D_g, liquid_salinity)
+    )
+
+
+def calculate_advective_salt_flux(liquid_salinity, Wl, cfg):
+    C = cfg.physical_params.concentration_ratio
+    return upwind(liquid_salinity + C, Wl)
+
+
+def calculate_frame_advection_salt_flux(salt, V):
+    return upwind(salt, V)
+
+
+def calculate_salt_flux(state_BCs, Wl, V, D_g, cfg):
+    liquid_salinity = state_BCs.liquid_salinity
+    liquid_fraction = state_BCs.liquid_fraction
+    salt = state_BCs.salt
+    salt_flux = (
+        calculate_diffusive_salt_flux(liquid_salinity, liquid_fraction, D_g, cfg)
+        + calculate_advective_salt_flux(liquid_salinity, Wl, cfg)
+        + calculate_frame_advection_salt_flux(salt, V)
+    )
+    return salt_flux
+
+
+def calculate_diffusive_gas_flux(dissolved_gas, liquid_fraction, D_g, cfg):
+    chi = cfg.physical_params.expansion_coefficient
+    lewis_gas = cfg.physical_params.lewis_gas
+    return (
+        -(chi / lewis_gas) * geometric(liquid_fraction) * np.matmul(D_g, dissolved_gas)
+    )
+
+
+def calculate_bubble_gas_flux(gas_fraction, Vg):
+    return upwind(gas_fraction, Vg)
+
+
+def calculate_advective_dissolved_gas_flux(dissolved_gas, Wl, cfg):
+    chi = cfg.physical_params.expansion_coefficient
+    return chi * upwind(dissolved_gas, Wl)
+
+
+def calculate_frame_advection_gas_flux(gas, V):
+    return upwind(gas, V)
+
+
+def calculate_gas_flux(state_BCs, Wl, V, Vg, D_g, cfg):
+    dissolved_gas = state_BCs.dissolved_gas
+    liquid_fraction = state_BCs.liquid_fraction
+    gas_fraction = state_BCs.gas_fraction
+    gas = state_BCs.gas
+    gas_flux = (
+        calculate_diffusive_gas_flux(dissolved_gas, liquid_fraction, D_g, cfg)
+        + calculate_bubble_gas_flux(gas_fraction, Vg)
+        + calculate_advective_dissolved_gas_flux(dissolved_gas, Wl, cfg)
+        + calculate_frame_advection_gas_flux(gas, V)
+    )
+    return gas_flux
--- celestine/solvers/lagged_solver.py
+++ celestine/solvers/lagged_solver.py
@@ -1,14 +1,10 @@
 import numpy as np
-from celestine.grids import (
-    upwind,
-    centers_to_edges,
-)
 from celestine.velocities import (
     calculate_velocities,
     calculate_absolute_permeability,
     solve_pressure_equation,
 )
-from celestine.flux import calculate_heat_flux
+from celestine.flux import calculate_heat_flux, calculate_salt_flux, calculate_gas_flux
 from celestine.solvers.template import SolverTemplate, State, StateBCs
 
 
@@ -37,9 +33,6 @@ class LaggedUpwindSolver(SolverTemplate):
         I = self.I
         timestep = cfg.numerical_params.timestep
 
-        chi = cfg.physical_params.expansion_coefficient
-        C = cfg.physical_params.concentration_ratio
-
         D_g = self.D_g
         D_e = self.D_e
 
@@ -48,45 +41,22 @@ class LaggedUpwindSolver(SolverTemplate):
 
         # calculate temperature, salinity etc for state on grid centers
         state.calculate_enthalpy_method(cfg)
-        pressure = state.pressure
-        liquid_fraction = state.liquid_fraction
-        gas_fraction = state.gas_fraction
 
         state_BCs = StateBCs(state, cfg)
 
-        dissolved_gas_ghosts = state_BCs.dissolved_gas
-        gas_fraction_ghosts = state_BCs.gas_fraction
-        gas_ghosts = state_BCs.gas
-        liquid_salinity_ghosts = state_BCs.liquid_salinity
-        salt_ghosts = state_BCs.salt
+        Vg, Wl, V = calculate_velocities(
+            state.liquid_fraction, state.pressure, D_g, cfg
+        )
 
-        liquid_fraction_edges = centers_to_edges(liquid_fraction)
-        Vg, Wl, V = calculate_velocities(liquid_fraction, pressure, D_g, cfg)
-
-        new_salt = np.zeros((I,))
-        new_gas = np.zeros((I,))
         new_pressure = np.zeros((I,))
 
         heat_flux = calculate_heat_flux(state_BCs, Wl, V, D_g)
-        new_enthalpy = take_forward_euler_step(state.enthalpy, heat_flux, timestep, D_e)
+        salt_flux = calculate_salt_flux(state_BCs, Wl, V, D_g, cfg)
+        gas_flux = calculate_gas_flux(state_BCs, Wl, V, Vg, D_g, cfg)
 
-        new_salt = salt_ghosts[1:-1] + timestep * (
-            (1 / cfg.physical_params.lewis_salt)
-            * np.matmul(
-                D_e, liquid_fraction_edges * np.matmul(D_g, liquid_salinity_ghosts)
-            )
-            - np.matmul(D_e, upwind(salt_ghosts, V))
-            - np.matmul(D_e, upwind(liquid_salinity_ghosts + C, Wl))
-        )
-        new_gas = gas_ghosts[1:-1] + timestep * (
-            (chi / cfg.physical_params.lewis_gas)
-            * np.matmul(
-                D_e, liquid_fraction_edges * np.matmul(D_g, dissolved_gas_ghosts)
-            )
-            - np.matmul(D_e, upwind(gas_ghosts, V))
-            - np.matmul(D_e, upwind(gas_fraction_ghosts, Vg))
-            - np.matmul(D_e, upwind(chi * dissolved_gas_ghosts, Wl))
-        )
+        new_enthalpy = take_forward_euler_step(state.enthalpy, heat_flux, timestep, D_e)
+        new_salt = take_forward_euler_step(state.salt, salt_flux, timestep, D_e)
+        new_gas = take_forward_euler_step(state.gas, gas_flux, timestep, D_e)
 
         new_state = State(
             new_time,
@@ -95,6 +65,7 @@ class LaggedUpwindSolver(SolverTemplate):
             new_gas,
         )
         new_state.calculate_enthalpy_method(cfg)
+        gas_fraction = state.gas_fraction
 
         new_liquid_fraction = new_state.liquid_fraction
         new_gas_fraction = new_state.gas_fraction
