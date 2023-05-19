9720de142b5f32e2b3e98bec44af3267d31b1d2b
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-05-19 12:17:51 +0100

    Prevent gas motion into gas saturated cell in reduced model

--- celestine/solvers/reduced_solver.py
+++ celestine/solvers/reduced_solver.py
@@ -1,3 +1,4 @@
+import numpy as np
 from celestine.velocities import (
     calculate_velocities,
 )
@@ -5,6 +6,31 @@ from celestine.flux import calculate_heat_flux, calculate_salt_flux, calculate_g
 from celestine.state import State, StateBCs
 from celestine.solvers.template import SolverTemplate
 from celestine.solvers.lagged_solver import take_forward_euler_step
+
+
+def prevent_gas_rise_into_saturated_cell(Vg, state_BCs: StateBCs):
+    """Modify the gas interstitial velocity to prevent bubble rise into a cell which
+    is already theoretically saturated with gas.
+
+    From the state with boundary conditions calculate the gas and solid fraction in the
+    cells (except at lower ghost cell). If any of these are such that there is more gas
+    fraction than pore space available then set gas insterstitial velocity to zero on
+    the edge below. Make sure the very top boundary velocity is not changed as we want
+    to always alow flux to the atmosphere regardless of the boundary conditions imposed.
+
+    :param Vg: gas insterstitial velocity on cell edges
+    :type Vg: Numpy array (size I+1)
+    :param state_BCs: state of system with boundary conditions
+    :type state_BCs: celestine.state.StateBCs
+    :return: filtered gas interstitial velocities on edges to prevent gas rise into a
+        fully gas saturated cell
+
+    """
+    gas_fraction_above = state_BCs.gas_fraction[1:]
+    solid_fraction_above = 1 - state_BCs.liquid_fraction[1:]
+    filtered_Vg = np.where(gas_fraction_above + solid_fraction_above >= 1, 0, Vg)
+    filtered_Vg[-1] = Vg[-1]
+    return filtered_Vg
 
 
 class ReducedSolver(SolverTemplate):
@@ -28,6 +54,7 @@ class ReducedSolver(SolverTemplate):
         state_BCs = StateBCs(state)  # Add boundary conditions
 
         Vg, Wl, V = calculate_velocities(state_BCs, D_g, cfg)
+        Vg = prevent_gas_rise_into_saturated_cell(Vg, state_BCs)
 
         heat_flux = calculate_heat_flux(state_BCs, Wl, V, D_g)
         salt_flux = calculate_salt_flux(state_BCs, Wl, V, D_g, cfg)
