4174e516e844617678e6b62aa5c77d098d150712
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-05-19 10:54:55 +0100

    Add reduced model solver

---
+++ celestine/solvers/reduced_solver.py
@@ -0,0 +1,47 @@
+from celestine.velocities import (
+    calculate_velocities,
+)
+from celestine.flux import calculate_heat_flux, calculate_salt_flux, calculate_gas_flux
+from celestine.state import State, StateBCs
+from celestine.solvers.template import SolverTemplate
+from celestine.solvers.lagged_solver import take_forward_euler_step
+
+
+class ReducedSolver(SolverTemplate):
+    """Take timestep using upwind scheme with liquid velocity calculation lagged."""
+
+    def pre_solve_checks(self):
+        self.cfg.check_thermal_Courant_number()
+
+    def take_timestep(self, state: State):
+        cfg = self.cfg
+        timestep = cfg.numerical_params.timestep
+
+        D_g = self.D_g
+        D_e = self.D_e
+
+        time = state.time
+        new_time = time + timestep
+
+        # calculate temperature, salinity etc for state on grid centers
+        state.calculate_enthalpy_method()
+        state_BCs = StateBCs(state)  # Add boundary conditions
+
+        Vg, Wl, V = calculate_velocities(state_BCs, D_g, cfg)
+
+        heat_flux = calculate_heat_flux(state_BCs, Wl, V, D_g)
+        salt_flux = calculate_salt_flux(state_BCs, Wl, V, D_g, cfg)
+        gas_flux = calculate_gas_flux(state_BCs, Wl, V, Vg, D_g, cfg)
+
+        new_enthalpy = take_forward_euler_step(state.enthalpy, heat_flux, timestep, D_e)
+        new_salt = take_forward_euler_step(state.salt, salt_flux, timestep, D_e)
+        new_gas = take_forward_euler_step(state.gas, gas_flux, timestep, D_e)
+
+        new_state = State(
+            cfg,
+            new_time,
+            new_enthalpy,
+            new_salt,
+            new_gas,
+        )
+        return new_state
