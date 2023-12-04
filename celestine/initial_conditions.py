071dc1a2eda9c7161857cb8e7262f37488664a7d
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-12-04 11:31:50 +0000

    Change bottom initial temperature condition in barrow simulation.
    Make it more similar to measured barrow bottom temperature profile.

--- celestine/initial_conditions.py
+++ celestine/initial_conditions.py
@@ -65,7 +65,7 @@ def get_barrow_initial_conditions(cfg: Config):
     """
     ICE_DEPTH = cfg.scales.convert_from_dimensional_grid(0.7)
     SALT_IN_ICE = cfg.scales.convert_from_dimensional_bulk_salinity(5.92)
-    BOTTOM_TEMP = cfg.boundary_conditions_config.far_temp
+    BOTTOM_TEMP = cfg.scales.convert_from_dimensional_temperature(-1.8)
     BOTTOM_SALT = cfg.boundary_conditions_config.far_bulk_salinity
     TEMP_IN_ICE = cfg.scales.convert_from_dimensional_temperature(-8.15)
 
