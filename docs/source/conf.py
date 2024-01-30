0e00492607375f26c0097ec74728eb912c2f3454
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-01-30 14:10:41 +0000

    Bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Joseph Fishlock"
 author = "Joseph Fishlock"
 
 # The full version, including alpha/beta/rc tags
-release = "0.11.0"
+release = "0.12.0"
 
 
 # -- General configuration ---------------------------------------------------
