7b336db7c9d9bddcec5ca350cba09dd2f8631d84
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-23 13:40:37 +0100

    Add parameterised test to test the solve method on a variety of configs
    At the moment test the default values of Config object, barrow
    temperature forcing and barrow temeprature forcing with brine drainage
    turned on and the bubble coupling to horizontal flow turned off.
    
    Each simulation is run only for 1 day of real world time just to check
    no major problems so these tests remain fast.

---
+++ tests/test_run_simulation.py
@@ -0,0 +1,43 @@
+import pytest
+from celestine.run_simulation import solve
+from celestine.dimensional_params import DimensionalParams
+
+
+@pytest.mark.parametrize(
+    "simulation_parameters",
+    [
+        {
+            "name": "defaults",
+            "total_time_in_days": 1,
+            "savefreq_in_days": 0.1,
+        },
+        {
+            "name": "barrow_forcing",
+            "total_time_in_days": 1,
+            "savefreq_in_days": 0.1,
+            "temperature_forcing_choice": "barrow_2009",
+            "Barrow_top_temperature_data_choice": "top_ice",
+        },
+        # {
+        #     # This will fail because of the coupling to horizontal flow is True
+        #     "name": "barrow_brine_drainage_defaults",
+        #     "total_time_in_days": 1,
+        #     "savefreq_in_days": 0.1,
+        #     "temperature_forcing_choice": "barrow_2009",
+        #     "Barrow_top_temperature_data_choice": "top_ice",
+        #     "brine_convection_parameterisation": True,
+        # },
+        {
+            "name": "barrow_brine_drainage_no_horizontal",
+            "total_time_in_days": 1,
+            "savefreq_in_days": 0.1,
+            "temperature_forcing_choice": "barrow_2009",
+            "Barrow_top_temperature_data_choice": "top_ice",
+            "brine_convection_parameterisation": True,
+            "couple_bubble_to_horizontal_flow": False,
+        },
+    ],
+)
+def test_short_solve(tmp_path, simulation_parameters):
+    print(tmp_path)
+    solve(DimensionalParams(**simulation_parameters).get_config(), tmp_path)
