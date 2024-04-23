594b1b6a1b9d7d49aa5c6527aa2b517dbf726b5d
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-23 14:28:40 +0100

    Make best_barrow test put output in tmp_path.
    Assert that it actually finishes the simulation and creates the output
    file best_barrow.npz

--- tests/test_best_barrow.py
+++ tests/test_best_barrow.py
@@ -2,21 +2,22 @@
 Test running best configuration for barrow configuration with brine convection
 and power law gas bubble size distribution.
 
-Run using the CLI interface with the single configuration and dimensional options
+Run using the CLI with dimensional option
 """
 
 from pathlib import Path
 import subprocess
 import pytest
 
-TEST_CONFIG_PATH = (
-    Path(__file__).parent / "test_configurations/best_barrow_dimensional.yml"
-)
+TEST_CONFIG_PATH = Path(__file__).parent / "test_configurations/best_barrow"
 
 
 @pytest.mark.slow
-def test_running_best_Barrow():
+def test_running_best_Barrow(tmp_path):
     subprocess.run(
-        f"python -m celestine --single {TEST_CONFIG_PATH} test_data_best_barrow --dimensional",
+        f"python -m celestine {TEST_CONFIG_PATH} {tmp_path} --dimensional",
         shell=True,
     )
+    # Test that the simulation has actually run and saved output
+    output_file = tmp_path / "best_barrow.npz"
+    assert output_file.is_file()
