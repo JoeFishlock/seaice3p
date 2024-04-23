0e5745c6b4eb9ffab77bf73791216cd6f517efaf
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-23 13:57:13 +0100

    Put output from YearlyConstant config test in tempdir.
    Don't need to generate the output.
    test also now checks that each simulation produces output file.
    If you actually want the output just run the program on the test config
    directory.

--- tests/test_YearlyConstantConfigs.py
+++ tests/test_YearlyConstantConfigs.py
@@ -8,12 +8,16 @@ interface for non-dimensional configurations.
 from pathlib import Path
 import subprocess
 import pytest
+from glob import glob
 
 TEST_CONFIG_DIR = Path(__file__).parent / "test_configurations/YearlyConstant"
 
 
 @pytest.mark.slow
-def test_running_YearlyConstant_configs():
-    subprocess.run(
-        f"python -m celestine {TEST_CONFIG_DIR} test_data_YearlyConstant", shell=True
+def test_running_YearlyConstant_configs(tmp_path):
+    subprocess.run(f"python -m celestine {TEST_CONFIG_DIR} {tmp_path}", shell=True)
+    # This command will exit successfully even if some of the simulations crashed
+    # test that it produced output for each config
+    assert len(glob(str(TEST_CONFIG_DIR / "*.yml"))) == len(
+        glob(str(tmp_path / "*.npz"))
     )
