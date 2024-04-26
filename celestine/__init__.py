1b4a746d91e92069e784223f1a9ffc503d1c2f48
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-26 11:57:08 +0100

    Add functions and classes package should export to init

--- celestine/__init__.py
+++ celestine/__init__.py
@@ -1,1 +1,17 @@
 __version__ = "0.14.0"
+
+# Exported functions and classes
+from .params import (
+    Config,
+    NumericalParams,
+    DarcyLawParams,
+    ForcingConfig,
+    BoundaryConditionsConfig,
+    PhysicalParams,
+)
+from .dimensional_params import DimensionalParams
+from .run_simulation import solve, run_batch
+from .state import get_model
+from .forcing import get_bottom_temperature_forcing, get_temperature_forcing
+from .grids import initialise_grids
+from .RJW14.brine_drainage import calculate_ice_ocean_boundary_depth
