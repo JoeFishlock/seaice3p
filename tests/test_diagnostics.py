94b8462228c3d6443cc12f7c846fddce76add379
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-05-07 12:15:45 +0100

    Add test to run new diagnostic script

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -16,6 +16,13 @@ def test_running_gas_velocity(tmp_path):
     main(tmp_path)
 
 
+@pytest.mark.slow
+def test_running_shortwave_case(tmp_path):
+    from celestine.diagnostics.shortwave_case import main, SIMULATION_DIMENSIONAL_PARAMS
+
+    main(tmp_path, tmp_path, SIMULATION_DIMENSIONAL_PARAMS)
+
+
 def test_running_brine_convection_parameterisation(tmp_path):
     from celestine.diagnostics.brine_drainage_parameterisation import main
 
