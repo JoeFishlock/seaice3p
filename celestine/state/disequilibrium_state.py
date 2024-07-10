7e33a4560d31e79eac9650f62080c7ccd6e87c60
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-07-10 11:31:05 +0100

    Create separate state class for DISEQ once enthalpy method is run

--- celestine/state/disequilibrium_state.py
+++ celestine/state/disequilibrium_state.py
@@ -45,9 +45,61 @@ class DISEQState:
         return self.bulk_dissolved_gas + self.gas_fraction
 
 
-def get_state_with_bcs(self):
+@dataclass(frozen=True)
+class DISEQStateFull:
+    """Contains all variables variables for solution with non-equilibrium gas phase
+    after running the enthalpy method on DISEQSate.
+    The total bulk gas is partitioned between dissolved gas and free phase gas with
+    a finite nucleation rate (non dimensional damkohler number).
+
+    principal solution components:
+    bulk enthalpy
+    bulk salinity
+    bulk dissolved gas
+    gas fraction
+
+    enthalpy method variables:
+    temperature
+    liquid_fraction
+    solid_fraction
+    liquid_salinity
+    dissolved_gas
+
+    all on the center grid.
+
+    Note:
+    Define bulk dissolved gas for the system as
+
+    expansion_coefficient * liquid_fraction * dissolved_gas
+
+    so that this is different from the dissolved gas concentration and
+
+    bulk_gas = bulk_dissolved_gas + gas_fraction
+
+    in non-dimensional units.
+    """
+
+    time: float
+    enthalpy: NDArray
+    salt: NDArray
+    bulk_dissolved_gas: NDArray
+    gas_fraction: NDArray
+
+    temperature: NDArray
+    liquid_fraction: NDArray
+    solid_fraction: NDArray
+    liquid_salinity: NDArray
+    dissolved_gas: NDArray
+
+    @cached_property
+    def gas(self) -> NDArray:
+        """Calculate bulk gas content and use same attribute name as EQMState"""
+        return self.bulk_dissolved_gas + self.gas_fraction
+
+
+def get_state_with_bcs(full_state: DISEQStateFull):
     """Initialise the appropriate StateBCs object"""
-    return DISEQStateBCs(self)
+    return DISEQStateBCs(full_state)
 
 
 def init_from_stacked_state(cls, cfg: cp.Config, time, stacked_state):
