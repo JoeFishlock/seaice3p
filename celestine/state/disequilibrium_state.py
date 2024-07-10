e8c9efc093e4785813161e40ab5222cc4a2da0b0
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-07-10 11:10:07 +0100

    !! Make DISEQState into a frozen dataclass with principal variables

--- celestine/state/disequilibrium_state.py
+++ celestine/state/disequilibrium_state.py
@@ -1,67 +1,83 @@
+from functools import cached_property
+from dataclasses import dataclass
 import numpy as np
+from numpy.typing import NDArray
 import celestine.params as cp
 from ..enthalpy_method import DisequilibriumEnthalpyMethod
-from .abstract_state import State
 from .disequilibrium_state_bcs import DISEQStateBCs
 
 
-class DISEQState(State):
-    """Stores information needed for solution at one timestep on cell centers"""
+@dataclass(frozen=True)
+class DISEQState:
+    """Contains the principal variables for solution with non-equilibrium gas phase.
+    The total bulk gas is partitioned between dissolved gas and free phase gas with
+    a finite nucleation rate (non dimensional damkohler number).
 
-    def __init__(
-        self, cfg: cp.Config, time, enthalpy, salt, bulk_dissolved_gas, gas_fraction
-    ):
-        """Define bulk dissolved gas for the system as
+    principal solution components:
+    bulk enthalpy
+    bulk salinity
+    bulk dissolved gas
+    gas fraction
 
-        expansion_coefficient * liquid_fraction * dissolved_gas
+    all on the center grid.
 
-        so that this is different from the dissolved gas concentration and
+    Note:
+    Define bulk dissolved gas for the system as
 
-        bulk_gas = bulk_dissolved_gas + gas_fraction
+    expansion_coefficient * liquid_fraction * dissolved_gas
 
-        in non-dimensional units.
-        """
-        self.cfg = cfg
-        self.time = time
-        self.enthalpy = enthalpy
-        self.salt = salt
-        self.bulk_dissolved_gas = bulk_dissolved_gas
-        self.gas_fraction = gas_fraction
+    so that this is different from the dissolved gas concentration and
 
-        # Calculate bulk gas content and use same attribute name as EQM model
-        self.gas = bulk_dissolved_gas + gas_fraction
+    bulk_gas = bulk_dissolved_gas + gas_fraction
 
-    def get_state_with_bcs(self):
-        """Initialise the appropriate StateBCs object"""
-        return DISEQStateBCs(self)
+    in non-dimensional units.
+    """
 
-    @classmethod
-    def init_from_stacked_state(cls, cfg: cp.Config, time, stacked_state):
-        """initialise from stacked solution vector for use in the solver"""
-        enthalpy, salt, bulk_dissolved_gas, gas_fraction = np.split(stacked_state, 4)
-        return cls(cfg, time, enthalpy, salt, bulk_dissolved_gas, gas_fraction)
+    time: float
+    enthalpy: NDArray
+    salt: NDArray
+    bulk_dissolved_gas: NDArray
+    gas_fraction: NDArray
 
-    def get_stacked_state(self):
-        return np.hstack(
-            (self.enthalpy, self.salt, self.bulk_dissolved_gas, self.gas_fraction)
-        )
+    @cached_property
+    def gas(self) -> NDArray:
+        """Calculate bulk gas content and use same attribute name as EQMState"""
+        return self.bulk_dissolved_gas + self.gas_fraction
 
-    def calculate_enthalpy_method(self):
-        (
-            temperature,
-            liquid_fraction,
-            gas_fraction,
-            solid_fraction,
-            liquid_salinity,
-            dissolved_gas,
-        ) = DisequilibriumEnthalpyMethod(
-            self.cfg.physical_params
-        ).calculate_enthalpy_method(
-            self
-        )
-        self.temperature = temperature
-        self.liquid_fraction = liquid_fraction
-        self.gas_fraction = gas_fraction
-        self.solid_fraction = solid_fraction
-        self.liquid_salinity = liquid_salinity
-        self.dissolved_gas = dissolved_gas
+
+def get_state_with_bcs(self):
+    """Initialise the appropriate StateBCs object"""
+    return DISEQStateBCs(self)
+
+
+def init_from_stacked_state(cls, cfg: cp.Config, time, stacked_state):
+    """initialise from stacked solution vector for use in the solver"""
+    enthalpy, salt, bulk_dissolved_gas, gas_fraction = np.split(stacked_state, 4)
+    return cls(cfg, time, enthalpy, salt, bulk_dissolved_gas, gas_fraction)
+
+
+def get_stacked_state(self):
+    return np.hstack(
+        (self.enthalpy, self.salt, self.bulk_dissolved_gas, self.gas_fraction)
+    )
+
+
+def calculate_enthalpy_method(self):
+    (
+        temperature,
+        liquid_fraction,
+        gas_fraction,
+        solid_fraction,
+        liquid_salinity,
+        dissolved_gas,
+    ) = DisequilibriumEnthalpyMethod(
+        self.cfg.physical_params
+    ).calculate_enthalpy_method(
+        self
+    )
+    self.temperature = temperature
+    self.liquid_fraction = liquid_fraction
+    self.gas_fraction = gas_fraction
+    self.solid_fraction = solid_fraction
+    self.liquid_salinity = liquid_salinity
+    self.dissolved_gas = dissolved_gas
