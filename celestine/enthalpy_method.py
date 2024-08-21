17bdf9d50c12dbb30889238818a95e9fb1f6cfc0
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-08-21 16:53:13 +0100

    Refactor enthalpy method and phase boundaries calculation

--- celestine/enthalpy_method.py
+++ celestine/enthalpy_method.py
@@ -2,26 +2,27 @@
 bulk salinity and bulk gas."""
 
 import numpy as np
-from abc import ABC, abstractmethod
 from celestine.params import PhysicalParams, Config
-from celestine.phase_boundaries import ReducedPhaseBoundaries
-from celestine.state.disequilibrium_state import DISEQStateFull
-from .state import EQMState, DISEQState, State, StateFull, EQMStateFull
+from celestine.phase_boundaries import get_phase_masks
+from .state import EQMState, DISEQState, State, StateFull, EQMStateFull, DISEQStateFull
 
 
 def calculate_enthalpy_method(cfg: Config, state: State) -> StateFull:
+    physical_params = cfg.physical_params
+    phase_masks = get_phase_masks(state, physical_params)
+    solid_fraction = _calculate_solid_fraction(state, physical_params, phase_masks)
+    liquid_fraction = _calculate_liquid_fraction(solid_fraction)
+    temperature = _calculate_temperature(
+        state, solid_fraction, physical_params, phase_masks
+    )
+    liquid_salinity = _calculate_liquid_salinity(state, temperature, phase_masks)
+    dissolved_gas = _calculate_dissolved_gas(
+        state, liquid_fraction, physical_params, phase_masks
+    )
+    gas_fraction = _calculate_gas_fraction(state, liquid_fraction, physical_params)
+
     match state:
         case EQMState():
-            (
-                temperature,
-                liquid_fraction,
-                gas_fraction,
-                solid_fraction,
-                liquid_salinity,
-                dissolved_gas,
-            ) = ReducedEnthalpyMethod(cfg.physical_params).calculate_enthalpy_method(
-                state
-            )
             return EQMStateFull(
                 state.time,
                 state.enthalpy,
@@ -35,18 +36,6 @@ def calculate_enthalpy_method(cfg: Config, state: State) -> StateFull:
                 gas_fraction,
             )
         case DISEQState():
-            (
-                temperature,
-                liquid_fraction,
-                gas_fraction,
-                solid_fraction,
-                liquid_salinity,
-                dissolved_gas,
-            ) = DisequilibriumEnthalpyMethod(
-                cfg.physical_params
-            ).calculate_enthalpy_method(
-                state
-            )
             return DISEQStateFull(
                 state.time,
                 state.enthalpy,
@@ -63,166 +52,115 @@ def calculate_enthalpy_method(cfg: Config, state: State) -> StateFull:
             raise NotImplementedError
 
 
-class EnthalpyMethod(ABC):
-    """Template for an enthalpy method. To implement a new method overwrite the
-    initializer to initialise the physical parameters and a suitable phase boundaries
-    object. Then implement a calculate enthalpy method that takes a state and uses bulk
-    enthalpy, salt and gas to return (temperature, liquid_fraction, gas_fraction,
-    solid_fraction, liquid_salinity, dissolved_gas).
-    """
+def _calculate_solid_fraction(state, physical_params: PhysicalParams, phase_masks):
+    enthalpy, salt = state.enthalpy, state.salt
+    solid_fraction = np.full_like(enthalpy, np.NaN)
+    L, M, E, S = phase_masks
+    St = physical_params.stefan_number
+    conc = physical_params.concentration_ratio
 
-    @abstractmethod
-    def __init__(self, physical_params: PhysicalParams):
-        pass
+    A = St
+    B = enthalpy[M] - St - conc
+    C = -(enthalpy[M] + salt[M])
 
-    @abstractmethod
-    def calculate_enthalpy_method(self, state):
-        pass
+    solid_fraction[L] = 0
+    solid_fraction[M] = (1 / (2 * A)) * (-B - np.sqrt(B**2 - 4 * A * C))
+    solid_fraction[E] = -(1 + enthalpy[E]) / St
+    solid_fraction[S] = 1
 
-
-class ReducedEnthalpyMethod(EnthalpyMethod):
-    def __init__(self, physical_params: PhysicalParams):
-        """initialise with physical parameters and the full phase boundaries calculator"""
-        self.physical_params = physical_params
-        self.phase_boundaries = ReducedPhaseBoundaries(physical_params)
-
-    def calculate_solid_fraction(self, enthalpy, salt, phase_masks):
-        solid_fraction = np.full_like(enthalpy, np.NaN)
-        L, M, E, S = phase_masks
-        St = self.physical_params.stefan_number
-        conc = self.physical_params.concentration_ratio
-
-        A = St
-        B = enthalpy[M] - St - conc
-        C = -(enthalpy[M] + salt[M])
-
-        solid_fraction[L] = 0
-        solid_fraction[M] = (1 / (2 * A)) * (-B - np.sqrt(B**2 - 4 * A * C))
-        solid_fraction[E] = -(1 + enthalpy[E]) / St
-        solid_fraction[S] = 1
-
-        return solid_fraction
-
-    def calculate_temperature(self, enthalpy, solid_fraction, phase_masks):
-        L, M, E, S = phase_masks
-        St = self.physical_params.stefan_number
-
-        temperature = np.full_like(enthalpy, np.NaN)
-        temperature[L] = enthalpy[L]
-        temperature[M] = enthalpy[M] + solid_fraction[M] * St
-        temperature[E] = -1
-        temperature[S] = enthalpy[S] + St
-
-        return temperature
-
-    def calculate_liquid_fraction(self, solid_fraction):
-        return 1 - solid_fraction
-
-    def calculate_gas_fraction(self, gas, liquid_fraction):
-        chi = self.physical_params.expansion_coefficient
-        tolerable_super_saturation = (
-            self.physical_params.tolerable_super_saturation_fraction
-        )
-        gas_fraction = np.full_like(liquid_fraction, np.NaN)
-
-        gas_sat = chi * liquid_fraction * tolerable_super_saturation
-        is_super_saturated = gas >= gas_sat
-        is_sub_saturated = ~is_super_saturated
-        gas_fraction[is_super_saturated] = (
-            gas[is_super_saturated] - gas_sat[is_super_saturated]
-        )
-        gas_fraction[is_sub_saturated] = 0
-        return gas_fraction
-
-    def calculate_dissolved_gas(self, gas, liquid_fraction):
-        chi = self.physical_params.expansion_coefficient
-        tolerable_super_saturation = (
-            self.physical_params.tolerable_super_saturation_fraction
-        )
-        dissolved_gas = np.full_like(gas, np.NaN)
-
-        gas_sat = chi * liquid_fraction * tolerable_super_saturation
-        is_super_saturated = gas >= gas_sat
-        is_sub_saturated = ~is_super_saturated
-        dissolved_gas[is_super_saturated] = tolerable_super_saturation
-        dissolved_gas[is_sub_saturated] = gas[is_sub_saturated] / (
-            chi * liquid_fraction[is_sub_saturated]
-        )
-        return dissolved_gas
-
-    def calculate_liquid_salinity(self, salt, temperature, phase_masks):
-        liquid_salinity = np.full_like(salt, np.NaN)
-        L, M, E, S = phase_masks
-
-        liquid_salinity[L] = salt[L]
-        liquid_salinity[M] = -temperature[M]
-        liquid_salinity[E] = 1
-        liquid_salinity[S] = 1
-
-        return liquid_salinity
-
-    def calculate_enthalpy_method(self, state):
-        phase_masks = self.phase_boundaries.get_phase_masks(state)
-        enthalpy, salt, gas = state.enthalpy, state.salt, state.gas
-        solid_fraction = self.calculate_solid_fraction(enthalpy, salt, phase_masks)
-        liquid_fraction = self.calculate_liquid_fraction(solid_fraction)
-        temperature = self.calculate_temperature(enthalpy, solid_fraction, phase_masks)
-        gas_fraction = self.calculate_gas_fraction(gas, liquid_fraction)
-        liquid_salinity = self.calculate_liquid_salinity(salt, temperature, phase_masks)
-        dissolved_gas = self.calculate_dissolved_gas(gas, liquid_fraction)
-        return (
-            temperature,
-            liquid_fraction,
-            gas_fraction,
-            solid_fraction,
-            liquid_salinity,
-            dissolved_gas,
-        )
+    return solid_fraction
 
 
-class DisequilibriumEnthalpyMethod(ReducedEnthalpyMethod):
-    """IGNORE TOLERABLE SUPERSATURATION PARAMETER FOR THIS IMPLEMENTATION"""
+def _calculate_temperature(
+    state, solid_fraction, physical_params: PhysicalParams, phase_masks
+):
+    enthalpy = state.enthalpy
+    L, M, E, S = phase_masks
+    St = physical_params.stefan_number
 
-    def calculate_gas_fraction(self):
-        raise TypeError("No need to call this as gas fraction is a primary variable")
+    temperature = np.full_like(enthalpy, np.NaN)
+    temperature[L] = enthalpy[L]
+    temperature[M] = enthalpy[M] + solid_fraction[M] * St
+    temperature[E] = -1
+    temperature[S] = enthalpy[S] + St
 
-    def calculate_dissolved_gas(self, bulk_dissolved_gas, liquid_fraction, phase_masks):
-        chi = self.physical_params.expansion_coefficient
-        L, M, E, S = phase_masks
+    return temperature
 
-        # prevent dissolved gas concentration blowing up during total freezing
-        REGULARISATION = 1e-6
 
-        dissolved_gas = np.full_like(bulk_dissolved_gas, np.NaN)
-        dissolved_gas[L] = bulk_dissolved_gas[L] / chi
-        dissolved_gas[M] = bulk_dissolved_gas[M] / (chi * liquid_fraction[M])
-        dissolved_gas[E] = bulk_dissolved_gas[E] / (
-            chi * liquid_fraction[E] + REGULARISATION
-        )
-        dissolved_gas[S] = 0
+def _calculate_liquid_fraction(solid_fraction):
+    return 1 - solid_fraction
 
-        return dissolved_gas
 
-    def calculate_enthalpy_method(self, state):
-        phase_masks = self.phase_boundaries.get_phase_masks(state)
-        enthalpy, salt, bulk_dissolved_gas, gas_fraction = (
-            state.enthalpy,
-            state.salt,
-            state.bulk_dissolved_gas,
-            state.gas_fraction,
-        )
-        solid_fraction = self.calculate_solid_fraction(enthalpy, salt, phase_masks)
-        liquid_fraction = self.calculate_liquid_fraction(solid_fraction)
-        temperature = self.calculate_temperature(enthalpy, solid_fraction, phase_masks)
-        liquid_salinity = self.calculate_liquid_salinity(salt, temperature, phase_masks)
-        dissolved_gas = self.calculate_dissolved_gas(
-            bulk_dissolved_gas, liquid_fraction, phase_masks
-        )
-        return (
-            temperature,
-            liquid_fraction,
-            gas_fraction,
-            solid_fraction,
-            liquid_salinity,
-            dissolved_gas,
-        )
+def _calculate_liquid_salinity(state, temperature, phase_masks):
+    salt = state.salt
+    liquid_salinity = np.full_like(salt, np.NaN)
+    L, M, E, S = phase_masks
+
+    liquid_salinity[L] = salt[L]
+    liquid_salinity[M] = -temperature[M]
+    liquid_salinity[E] = 1
+    liquid_salinity[S] = 1
+
+    return liquid_salinity
+
+
+def _calculate_gas_fraction(state, liquid_fraction, physical_params: PhysicalParams):
+    match state:
+        case EQMState():
+            chi = physical_params.expansion_coefficient
+            tolerable_super_saturation = (
+                physical_params.tolerable_super_saturation_fraction
+            )
+            gas_fraction = np.full_like(liquid_fraction, np.NaN)
+
+            gas_sat = chi * liquid_fraction * tolerable_super_saturation
+            is_super_saturated = state.gas >= gas_sat
+            is_sub_saturated = ~is_super_saturated
+            gas_fraction[is_super_saturated] = (
+                state.gas[is_super_saturated] - gas_sat[is_super_saturated]
+            )
+            gas_fraction[is_sub_saturated] = 0
+            return gas_fraction
+        case DISEQState():
+            return state.gas_fraction
+        case _:
+            raise NotImplementedError
+
+
+def _calculate_dissolved_gas(
+    state, liquid_fraction, physical_params: PhysicalParams, phase_masks
+):
+    chi = physical_params.expansion_coefficient
+    match state:
+        case EQMState():
+            gas = state.gas
+            tolerable_super_saturation = (
+                physical_params.tolerable_super_saturation_fraction
+            )
+            dissolved_gas = np.full_like(gas, np.NaN)
+
+            gas_sat = chi * liquid_fraction * tolerable_super_saturation
+            is_super_saturated = gas >= gas_sat
+            is_sub_saturated = ~is_super_saturated
+            dissolved_gas[is_super_saturated] = tolerable_super_saturation
+            dissolved_gas[is_sub_saturated] = gas[is_sub_saturated] / (
+                chi * liquid_fraction[is_sub_saturated]
+            )
+            return dissolved_gas
+        case DISEQState():
+            L, M, E, S = phase_masks
+            bulk_dissolved_gas = state.bulk_dissolved_gas
+
+            # prevent dissolved gas concentration blowing up during total freezing
+            REGULARISATION = 1e-6
+
+            dissolved_gas = np.full_like(bulk_dissolved_gas, np.NaN)
+            dissolved_gas[L] = bulk_dissolved_gas[L] / chi
+            dissolved_gas[M] = bulk_dissolved_gas[M] / (chi * liquid_fraction[M])
+            dissolved_gas[E] = bulk_dissolved_gas[E] / (
+                chi * liquid_fraction[E] + REGULARISATION
+            )
+            dissolved_gas[S] = 0
+            return dissolved_gas
+        case _:
+            raise NotImplementedError
--- celestine/phase_boundaries.py
+++ celestine/phase_boundaries.py
@@ -1,57 +1,46 @@
-"""Module for calculating the phase boundaries needed for the enthalpy method.
-"""
+r"""Module for calculating the phase boundaries needed for the enthalpy method.
 
-import numpy as np
-from abc import ABC, abstractmethod
-from celestine.params import PhysicalParams
-
-
-class PhaseBoundaries(ABC):
-    """Template for phase boundary calculation.
-
-    Concrete implementations should use the state containing enthalpy, salt and gas to
-    calculate the liquidus, enthalpy, solidus and saturation boundaries and then return
-    masks for each possible phase of the system.
-    """
-
-    def __init__(self, physical_params: PhysicalParams):
-        self.physical_params = physical_params
-
-    @abstractmethod
-    def get_phase_masks(self, state):
-        pass
-
-
-class ReducedPhaseBoundaries(PhaseBoundaries):
-    r"""calculates the phase boundaries neglecting the gas fraction so that
+calculates the phase boundaries neglecting the gas fraction so that
 
     .. math:: \phi_s + \phi_l = 1
 
-    """
+"""
 
-    def calculate_liquidus(self, salt):
-        return -salt
+import numpy as np
+from .state import State
+from .params import PhysicalParams
 
-    def calculate_eutectic(self, salt):
-        C = self.physical_params.concentration_ratio
-        St = self.physical_params.stefan_number
-        return (St * (salt - 1) / (1 + C)) - 1
 
-    def calculate_solidus(self, salt):
-        St = self.physical_params.stefan_number
-        return np.full_like(salt, -1 - St)
+def _calculate_liquidus(salt):
+    return -salt
 
-    def get_phase_masks(self, state):
-        enthalpy, salt = state.enthalpy, state.salt
-        liquidus = self.calculate_liquidus(salt)
-        eutectic = self.calculate_eutectic(salt)
-        solidus = self.calculate_solidus(salt)
-        is_liquid = enthalpy >= liquidus
-        is_mush = (enthalpy >= eutectic) & (enthalpy < liquidus)
-        is_eutectic = (enthalpy >= solidus) & (enthalpy < eutectic)
-        is_solid = enthalpy < solidus
-        L = is_liquid
-        M = is_mush
-        E = is_eutectic
-        S = is_solid
-        return L, M, E, S
+
+def _calculate_eutectic(salt, concentration_ratio, stefan_number):
+    C = concentration_ratio
+    St = stefan_number
+    return (St * (salt - 1) / (1 + C)) - 1
+
+
+def _calculate_solidus(salt, stefan_number):
+    St = stefan_number
+    return np.full_like(salt, -1 - St)
+
+
+def get_phase_masks(state: State, physical_params: PhysicalParams):
+    enthalpy, salt = state.enthalpy, state.salt
+    concentration_ratio, stefan_number = (
+        physical_params.concentration_ratio,
+        physical_params.stefan_number,
+    )
+    liquidus = _calculate_liquidus(salt)
+    eutectic = _calculate_eutectic(salt, concentration_ratio, stefan_number)
+    solidus = _calculate_solidus(salt, stefan_number)
+    is_liquid = enthalpy >= liquidus
+    is_mush = (enthalpy >= eutectic) & (enthalpy < liquidus)
+    is_eutectic = (enthalpy >= solidus) & (enthalpy < eutectic)
+    is_solid = enthalpy < solidus
+    L = is_liquid
+    M = is_mush
+    E = is_eutectic
+    S = is_solid
+    return L, M, E, S
