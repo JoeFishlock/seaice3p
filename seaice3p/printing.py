a86bec5399ba18f1c9a56534765137088a738100
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-11-04 15:46:43 +0000

    Add more verbosity options
    no verbosity flag prints nothing
    
    -v prints just the version and the name of the simulation being run
    (useful when running on cluster)
    
    -vv prints simulation name and current time (useful when only running
    one simulation at a time to monitor progress)
    
    This is done by adding a verbosity threshold argument to the get_printer
    function

--- seaice3p/printing.py
+++ seaice3p/printing.py
@@ -1,8 +1,10 @@
 from typing import Callable
 
 
-def get_printer(verbosity_level: int) -> Callable[[str], None]:
-    if verbosity_level >= 1:
+def get_printer(
+    verbosity_level: int, verbosity_threshold: int
+) -> Callable[[str], None]:
+    if verbosity_level >= verbosity_threshold:
 
         def optprint(message: str, **kwargs):
             print(message, **kwargs)
